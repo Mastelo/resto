9192bfb30057e2e9a125b700370a04d8e79d2e3e
github_username <you@domain.com>
2019-05-30 20:20:17 -0400

    grafica comida

---
+++ src/Formularios/DConexion.java
@@ -0,0 +1,34 @@
+
+package Formularios;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class DConexion {
+    
+     private final String base = "bdrest";
+    private final String user = "root";
+    private final String pass = "";
+    //private final String url  = "jdbc:mysql://localhost:3307/"+ base;
+    private final String url  = "jdbc:mysql://localhost:3307/bdrest?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+
+     Connection   con  = null;
+    
+    public Connection getConexion(){
+        
+        try {
+            //Class.forName("com.mysql.jbc.Driver");
+            con = DriverManager.getConnection(this.url, this.user, this.pass);
+            System.out.println("Connected!");
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+        return con;
+    }
+    
+    
+}
---
+++ src/Formularios/DGraficar.java
@@ -0,0 +1,66 @@
+
+package Formularios;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Date;
+import javax.swing.table.DefaultTableModel;
+
+public class DGraficar extends DConexion {
+    private FrmGraficas grafic;
+    
+    Connection cn = getConexion();
+    
+    public DefaultTableModel mostrarComida(){
+        
+        DefaultTableModel miModelo = null;
+        try{
+            String titulos []= {"Idcomida","Nombre"};
+            String dts [] = new String[2];
+            miModelo = new DefaultTableModel(null, titulos);
+            String sql = "select idc, nombre from comida1";
+            PreparedStatement pst = cn.prepareStatement(sql);
+            ResultSet rs = pst.executeQuery();
+            while(rs.next()){
+               dts[0] = rs.getString("idc");
+               dts[1] = rs.getString("Nombre");
+               miModelo.addRow(dts);
+            }
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return miModelo;
+    }
+    
+    public DefaultTableModel agruparComida(String fechaD, String fechaH){
+      
+        DefaultTableModel miModelo = null;
+       
+        try{
+            String titulos []= {"Nombre","TotalPedidos"};
+            String dts [] = new String[2];
+            miModelo = new DefaultTableModel(null, titulos);
+            String sql = "SELECT nombre, SUM(cantidad) AS TotalPedidos\n" +
+                         "FROM comida c, pedido p, producto_pedido pp\n" +
+                         "WHERE p.idpedido=pp.idpedido and pp.idcomida=c.idcomida and fechaPedido between '"+fechaD+"' and '"+fechaH+"'\n" +
+                         "GROUP BY c.nombre\n" +
+                         "ORDER BY SUM(cantidad) DESC\n" +
+                         "LIMIT 0 , 3";
+            PreparedStatement pst = cn.prepareStatement(sql);
+            ResultSet rs = pst.executeQuery();
+            while(rs.next()){
+               dts[0] = rs.getString("Nombre");
+               dts[1] = rs.getString("totalpedidos");
+               miModelo.addRow(dts);
+            }
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return miModelo;
+    }   
+    
+    
+    
+    
+}
